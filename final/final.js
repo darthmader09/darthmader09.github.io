3faf18555b551d1d0975c8906d327eebd71dad4d
darthmader09 <125585513+darthmader09@users.noreply.github.com>
2026-04-29 01:41:56 -0500

    Delete final/final.js

--- final/final.js
+++
@@ -1,2 +0,0 @@
-winRecordData() {
-alert("You are playing a " + "getElementById(color)" +" " + "getElementById(archetype)" + "deck. You have won " + "getElementById(wins)" + "/" + "getElementById(total)" + " games!"); }
